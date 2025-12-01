a46f3d85ce72e17c60c36e096743c9df2e58ff3f
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2025-12-01 09:21:50 -0600

    style: bold today header

--- src/features/journal/components/today-header.tsx
+++ src/features/journal/components/today-header.tsx
@@ -5,8 +5,8 @@ export const TodayHeader = () => {
 	const today = useDateNow();
 
 	return (
-		<span className="flex items-baseline gap-2 sticky top-app-top border-b border-dashed pl-app-left pr-app-right py-2 bg-black/70 backdrop-blur">
-			<span className="font-medium text-xl font-serif">
+		<span className="flex items-baseline mb-2 gap-2 sticky top-app-top border-b border-dashed pl-app-left pr-app-right py-2 bg-black/70 backdrop-blur">
+			<span className="font-bold text-xl font-serif">
 				{formatMonthDate(today.toISOString())}
 			</span>
 			<span className="text-sm text-white/70">

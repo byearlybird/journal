6e91fa4d7745dd8481f78adb45139fb1a61e0d2c
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2025-12-30 21:25:00 -0600

    add check script

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./styles/tokens.css";
 import "./global.css";
-import App from "./App.tsx";
 import { ClerkProvider } from "@clerk/clerk-react";
+import App from "./App.tsx";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as
@@ -15,6 +15,7 @@ if (!PUBLISHABLE_KEY) {
 	throw new Error("Add your Clerk Publishable Key to the .env file");
 }
 
+// biome-ignore lint/style/noNonNullAssertion: we know the element is always present
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<ClerkProvider publishableKey={PUBLISHABLE_KEY}>

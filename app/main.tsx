68507e1a07391d54ec55e7ac2b9945ecbe98ce7c
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2026-01-03 11:14:03 -0600

    init env

--- app/main.tsx
+++ app/main.tsx
@@ -1,18 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./main.css";
+import { ENV } from "@app/env";
 import { Root } from "@app/routes/_root";
 import { load } from "@app/store/persistence";
 import { ClerkProvider } from "@clerk/clerk-react";
-
-// Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as
-	| string
-	| undefined;
-
-if (!PUBLISHABLE_KEY) {
-	throw new Error("Add your Clerk Publishable Key to the .env file");
-}
 
 // Initialize store from IDB
 await load();
@@ -20,7 +12,7 @@ await load();
 // biome-ignore lint/style/noNonNullAssertion: we know the element is always present
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+		<ClerkProvider publishableKey={ENV.VITE_CLERK_PUBLISHABLE_KEY}>
 			<Root />
 		</ClerkProvider>
 	</StrictMode>,

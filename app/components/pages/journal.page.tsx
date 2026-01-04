b7a4e07f5207dd84b26195896752ae1b0c57ae59
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2026-01-03 23:13:59 -0600

    tdoay / all

--- app/components/pages/journal.page.tsx
+++ app/components/pages/journal.page.tsx
@@ -1,18 +1,40 @@
-import { useNotes } from "@app/store/queries";
+import { useNotes, useTodayNotes } from "@app/store/queries";
+import { Tabs } from "@base-ui/react/tabs";
 
 export function JournalPage() {
 	const notes = useNotes();
+	const todayNotes = useTodayNotes();
 
 	return (
-		<div className="flex flex-col gap-4 p-4">
-			{notes.map((note) => (
-				<article
-					key={note.id}
-					className="flex rounded border border-white bg-black p-4"
-				>
-					{note.content}
-				</article>
-			))}
-		</div>
+		<Tabs.Root defaultValue="today">
+			<Tabs.List className="flex gap-4 p-4">
+				<Tabs.Tab value="today" className="data-active:font-bold">
+					Today
+				</Tabs.Tab>
+				<Tabs.Tab value="all" className="data-active:font-bold">
+					All
+				</Tabs.Tab>
+			</Tabs.List>
+			<Tabs.Panel value="today" className="flex flex-col gap-4 p-4">
+				{todayNotes.map((note) => (
+					<article
+						key={note.id}
+						className="flex rounded border border-white bg-black p-4"
+					>
+						{note.content}
+					</article>
+				))}
+			</Tabs.Panel>
+			<Tabs.Panel value="all" className="flex flex-col gap-4 p-4">
+				{notes.map((note) => (
+					<article
+						key={note.id}
+						className="flex rounded border border-white bg-black p-4"
+					>
+						{note.content}
+					</article>
+				))}
+			</Tabs.Panel>
+		</Tabs.Root>
 	);
 }
--- app/store/queries.tsx
+++ app/store/queries.tsx
@@ -1,4 +1,5 @@
 import { useStore } from "@nanostores/react";
+import { isToday } from "date-fns";
 import { store } from ".";
 
 export const useNotes = () =>
@@ -8,5 +9,12 @@ export const useNotes = () =>
 				(a, b) =>
 					new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
 			),
+		),
+	);
+
+export const useTodayNotes = () =>
+	useStore(
+		store.query(["notes"], ({ notes }) =>
+			Array.from(notes.values()).filter((note) => isToday(note.createdAt)),
 		),
 	);
